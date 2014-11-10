89f517538dbf482d6009d0ac306d5e33c44d3b51
Christian Lund <pi@raspberrypi.(none)>
2014-11-10 12:58:22 +0000

    Removed code for multiple input files and simple plotting.

--- backend.py
+++ backend.py
@@ -52,18 +52,6 @@ def get_data_from_well(dataframe, well):
     return dataframe[dataframe['ScanArea.Name'] == well]
 
 
-def make_simple_plot(filename, wells, numbers, letters, measurement = 'BCA'):
-    df = get_dataframe(filename)
-    for n in range(len(wells)):
-        well_data = get_data_from_well(df, wells[n])
-        plt.plot(well_data['TimestampInSeconds'].values / 3600, well_data[measurement],\
-                 marker = markers[n], label = letters[wells[n][0]] + ', ' + numbers[wells[n][1:]] + ', ' + wells[n])
-    plt.ylabel(measurement)
-    plt.xlabel('Time (Hours)')
-    plt.legend(loc = 4)
-    plt.show()
-    plt.clf()
-
 def make_triplicates(filename, wellslist, numbers, letters, errorbars = 'SE', measurement = 'BCA', show = False):
     df = get_dataframe(filename)
     for i in range(len(wellslist)):
@@ -87,31 +75,4 @@ def make_triplicates(filename, wellslist, numbers, letters, errorbars = 'SE', me
     else:
         plt.savefig('static/showme.png')
     plt.clf()
-
-def make_combined_triplicates(filenames, wellslist, numbers, letters, measurement = 'BCA', show = False):
-    dataframes = [get_dataframe(x) for x in filenames]
-    for i in range(len(wellslist)):
-        wells = wellsList[i]
-        arrays = []
-        for n in range(len(dataframes)):
-            for well in wells[n]:
-                well_data = get_data_from_well(dataFrames[n], well)
-                if len(well_data.values) > 49:
-                    want = [x for x in range(len(well_data)) if (x < 24 and x % 2 == 0) or x >= 24]
-                    arrays.append(array([well_data[measurement].values[y] for y in want]))
-                else:
-                    arrays.append(well_data[measurement].values[:-1])
-        means = get_mean(arrays)
-        sds = calc_sd(arrays, means)
-        ses = calc_se(arrays, sds)
-        plt.errorbar(array(range(len(means))) / 2.0, means,\
-                     label = letters[wells[1][0][0]] + ', ' + numbers[wells[1][0][1:]],\
-                     yerr = sds, marker = markers[i], linewidth = 2)
-        plt.legend(loc = 4)
-    plt.xlabel('Time (hours)')
-    plt.ylabel(measurement)
-    if show:
-        plt.show()
-    else:
-        plt.savefig('static/showme.png')
-    plt.clf()
+   plt.clf()
